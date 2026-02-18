7f9caf760b355cc1b3a1cd6da78c33eec1e91b1c
Tullysaurus <tullypro69@gmail.com>
2026-02-18 16:07:42 -0600

    Update to use env database

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from schema import Base
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./uets_database.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./uets_database.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
